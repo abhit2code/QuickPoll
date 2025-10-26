8bc5ea3f296b611802f231e91394bdc99a7b6b8c
Abhit Rana <abhitrana@Abhits-MacBook-Air.local>
2025-10-27 04:11:26 +0530

    debugging

--- start_backend.sh
+++ start_backend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /Users/abhitrana/Documents/coding/quickpoll/backend
 export DATABASE_URL="postgresql://user:password@localhost:5432/quickpoll"
-export PATH="/Users/abhitrana/Library/Python/3.9/bin:$PATH"
-python3 -m uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+source venv/bin/activate
+python -m uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 --proxy-headers
